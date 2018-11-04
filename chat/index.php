93ba35f0e6f98769e408b9efe644ff81d71c9735
MarcosGrzeca <marcos-a-g@hotmail.com>
2018-11-03 22:26:47 -0300

    CHAT

--- chat/index.php
+++ chat/index.php
@@ -8,11 +8,11 @@ PASSOS
 <br/>
 <a href='importar_from_artigo.php'>Importar from planilha</a> FEITO
 <br/>
-<a href='coletar_drunk_tweets.php'>Coletar drunk tweets da API</a> RODANDO
+<a href='coletar_drunk_tweets.php'>Coletar drunk tweets da API</a> FEITO
 <br/>
-<a href='extrair_user_id.php'>Extrair user_id</a> RODANDO
+<a href='extrair_user_id.php'>Extrair user_id</a> FEITO
 <br/>
-<a href='coletar_sober_tweets.php'>Coletar sober tweets da API</a>
+<a href='coletar_sober_tweets.php'>Coletar sober tweets da API</a> RODANDO
 <br/>
 </body>
 </html>
