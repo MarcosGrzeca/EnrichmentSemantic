a2619920ea6823d3b26a894162b80830996b0fee
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-02-18 14:10:39 -0300

    zole planilhas

--- analisar_concordancia_amazonv2.php
+++ analisar_concordancia_amazonv2.php
@@ -2,7 +2,9 @@
 
 require_once("config.php");
 
-$files = array("planilhas/Batch_3523119_batch_results.csv", "planilhas/Batch_3515675_batch_results.csv", "planilhas/Batch_3521895_batch_results.csv", "planilhas/Batch_3521951_batch_results.csv", "planilhas/Batch_3521993_batch_results.csv", "planilhas/Batch_3523241_batch_results.csv", "planilhas/Batch_3524454_batch_results.csv", "planilhas/Batch_3526694_batch_results.csv", "planilhas/Batch_3526743_batch_results.csv", "planilhas/Batch_3527050_batch_results.csv", "planilhas/Batch_3527093_batch_results.csv", "planilhas/Batch_3527559_batch_results.csv", "planilhas/Batch_3527584_batch_results.csv", "planilhas/Batch_3527886_batch_results.csv", "planilhas/Batch_3531458_batch_results.csv", "planilhas/Batch_3534492_batch_results.csv");
+// $files = array("planilhas/Batch_3523119_batch_results.csv", "planilhas/Batch_3515675_batch_results.csv", "planilhas/Batch_3521895_batch_results.csv", "planilhas/Batch_3521951_batch_results.csv", "planilhas/Batch_3521993_batch_results.csv", "planilhas/Batch_3523241_batch_results.csv", "planilhas/Batch_3524454_batch_results.csv", "planilhas/Batch_3526694_batch_results.csv", "planilhas/Batch_3526743_batch_results.csv", "planilhas/Batch_3527050_batch_results.csv", "planilhas/Batch_3527093_batch_results.csv", "planilhas/Batch_3527559_batch_results.csv", "planilhas/Batch_3527584_batch_results.csv", "planilhas/Batch_3527886_batch_results.csv", "planilhas/Batch_3531458_batch_results.csv", "planilhas/Batch_3534492_batch_results.csv");
+// $files = array("planilhas/Batch_3535981_batch_results.csv"); //Faltando 1
+$files = array("planilhas/Batch_3536013_batch_results.csv"); //Faltando 2
 foreach ($files as $file) {
     $avaliacoes = [];
     if (($handle = fopen($file, "r")) !== FALSE) {
@@ -29,7 +31,7 @@ foreach ($files as $file) {
     foreach ($avaliacoes as $key => $linha) {
         $tweets = query("SELECT id, classificado FROM semantic_tweets_alcolic WHERE situacao = 1 AND link = '" . $key . "';");
         foreach (getRows($tweets) as $tweet) {
-            $aUpdate = array("classificado" => 1, "classificador" => "Amazon");
+            /*$aUpdate = array("classificado" => 1, "classificador" => "Amazon");
             if (isset($linha[0])) {
                 $aUpdate["a1"] = $linha[0];
             }
@@ -38,6 +40,12 @@ foreach ($files as $file) {
             }
             if (isset($linha[2])) {
                 $aUpdate["a3"] = $linha[2];
+            }*/
+            if (isset($linha[0])) {
+                $aUpdate["a2"] = $linha[0];
+            }
+            if (isset($linha[1])) {
+                $aUpdate["a3"] = $linha[1];
             }
             update("semantic_tweets_alcolic", $tweet["id"], $aUpdate);
         }
