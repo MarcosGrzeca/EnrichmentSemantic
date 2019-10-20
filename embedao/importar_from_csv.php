c4ab0e199c733b950b40ee158b09884112781ca8
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-10-20 14:08:51 -0300

    Teste embeddao

--- embedao/importar_from_csv.php
+++ embedao/importar_from_csv.php
@@ -5,17 +5,20 @@ set_time_limit(0);
 
 echo "<pre>";
 
-$files = glob("planilhas/emotion/*.csv");
+$files = glob("planilhas/tweetarchivist/*.csv");
 foreach ($files as $key => $value) {
     $value = trim($value);
     echo $value . PHP_EOL;
         
     if (($handle = fopen($value, "r")) !== FALSE) {
         $cont = 0;
-        while (($data = fgetcsv($handle, 2000, ",")) !== FALSE) {
+        while (($data = fgetcsv($handle, 4000, ";")) !== FALSE) {
             if ($cont != 0) {
+                if ($data[5] != "en") {
+                    continue;
+                }
                 try {
-                    insert("embeddao", array("id", "texto", "origem"), array($data[1], $data[5], "emotion"), false);
+                    insert("embeddao", array("id", "texto", "origem"), array($data[0], $data[4], "archivist"), false);
                 } catch (Exception $e) {
                     if ($e->getCode() == 1062) {
                         continue;
@@ -23,6 +26,9 @@ foreach ($files as $key => $value) {
                 }
             }
             $cont++;
+            if ($cont > 25) {
+                break;
+            }
         }
     }
 }
